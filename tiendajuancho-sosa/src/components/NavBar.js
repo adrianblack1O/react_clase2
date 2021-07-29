66ea4efc6000c1ec03a49137a87225cbe09cb75c
adrianblack1O <adrianblack10@gmail.com>
2021-07-29 10:22:44 -0700

    NavBar - Clase 3 (recomnedaciones)

--- tiendajuancho-sosa/src/components/NavBar.js
+++ tiendajuancho-sosa/src/components/NavBar.js
@@ -6,14 +6,14 @@ class NavBar extends React.Component {
     render() {
         return ( 
             <nav>
-            <div class="nav-wrapper light-blue darken-3">
-              <a href="#home" class="brand-logo light-blue darken-3"><img src="images/juancho.png" width="60" height="auto" alt="logo Juancho"/>TiendaJuancho</a>
-              <ul id="nav-mobile" class="right hide-on-med-and-down light-blue darken-3">
+            <div className="nav-wrapper light-blue darken-3">
+              <a href="#home" className="brand-logo light-blue darken-3"><img src="images/juancho.png" width="60" height="auto" alt="logo Juancho"/>TiendaJuancho</a>
+              <ul id="nav-mobile" className="right hide-on-med-and-down light-blue darken-3">
                 <li><a href="#home">Home</a></li>
                 <li><a href="#productos">Productos</a></li>
                 <li><a href="#about">Quienes Somos?</a></li>
                 <li><a href="#contacto">Contactenos</a></li>
-                <li><a href="#carrito"><i class="material-icons">shopping_cart</i></a> </li>
+                <li><a href="#carrito"><i className="material-icons">shopping_cart</i></a> </li>
               </ul>
             </div>
           </nav>
